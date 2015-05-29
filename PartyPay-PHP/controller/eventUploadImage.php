da6c6645787e24e3665951b0c2d3fafc513d29e2
fabriciosc92 <fabriciosc92@gmail.com>
2015-05-29 00:21:40 -0300

    Insert Constants

--- PartyPay-PHP/controller/eventUploadImage.php
+++ PartyPay-PHP/controller/eventUploadImage.php
@@ -3,14 +3,28 @@
 
 require 'canvas.php'
 	or log_it("Could not include canvas.php file");
+const NO_ERROR = 0;
+const MAX_FILE_SIZE = 2;
+const MAX_FILE_SIZE_TYPE = "MB";
+const FILE_SIZE_MULTIPLYER = 2; // KB = 1, MB = 2, GB = 3
+const INVALID_EXTENSION = false;
+const THUMBNAIL_WIDTH = 80;
+const THUMBNAIL_HEIGTH = 80;
+const THUMBNAIL_QUALITY = 100; // 1 to 100
+
+
+require 'canvas.php';
 // Pasta onde o arquivo vai ser salvo
 
 $_UP['pasta'] = 'view/images/';
 
 // Tamanho máximo do arquivo (em Bytes)
 
-$_UP['tamanho'] = 1024 * 1024 * 2; // 2Mb
+$_UP['tamanho'] = (FILE_SIZE_MULTIPLYER * 1024) * MAX_FILE_SIZE; // 2Mb
 // Array com as extensões permitidas
+
+echo($_UP['tamanho']);
+die();
 
 $_UP['extensoes'] = array('jpg', 'png', 'gif');
 
@@ -34,8 +48,8 @@ $_UP['erros'][4] = 'Não foi feito o upload do arquivo';
 
 // Verifica se houve algum erro com o upload. Se sim, exibe a mensagem do erro
 
-if ($_FILES['arquivo']['error'] != 0) {
-
+if ($_FILES['arquivo']['error'] != NO_ERROR) {
+	
 	log_it("Error on image upload: "$_UP['erros'][$_FILES['arquivo']['error']]);
 
     die("Não foi possível fazer o upload, erro:<br />" . $_UP['erros'][$_FILES['arquivo']['error']]);
@@ -51,10 +65,10 @@ $var = explode('.', $_FILES['arquivo']['name']);
 
 $extension = strtolower(end($var));
 
-if (array_search($extension, $_UP['extensoes']) === false) {
+if (array_search($extension, $_UP['extensoes']) === INVALID_EXTENSION) {
 	
 	log_it("Error on image upload: invalid file extension");
-	
+
     echo "Por favor, envie arquivos com as seguintes extensões: jpg, png ou gif";
 }
 
@@ -62,7 +76,7 @@ if (array_search($extension, $_UP['extensoes']) === false) {
 
 // Faz a verificação do tamanho do arquivo
 else if ($_UP['tamanho'] < $_FILES['arquivo']['size']) {
-
+	
 	log_it("Error on image upload: invalid file fize");
 
     echo "O arquivo enviado é muito grande, envie arquivos de até 2Mb.";
@@ -97,18 +111,14 @@ else {
         $event->setImagem($_UP['pasta'] . $finalName);
         $thumb = new canvas($event->getImagem());
 
-        $width = 80;
-        $heigth = 80;
-
-
-        $thumb->redimensiona($widht, $heigth, 'proporcional');
-        $thumb->grava($_UP['pasta'] . "thumb" . $finalName, 100);
+        $thumb->redimensiona(THUMBNAIL_WIDTH, THUMBNAIL_HEIGTH, 'proporcional');
+        $thumb->grava($_UP['pasta'] . "thumb" . $finalName, THUMBNAIL_QUALITY);
         $event->setMiniatura($_UP['pasta'] . "thumb" . $finalName);
     } else {
 
-// Não foi possível fazer o upload, provavelmente a pasta está incorreta
+// Não foi possível fazer o upload, provavelmente a pasta está incorreta		
 		log_it("Error on image upload: unable to move uploaded file");
-		
+
         echo "Não foi possível enviar o arquivo, tente novamente";
     }
 }
